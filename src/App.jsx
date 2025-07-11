c100de05f3888c5ec39a45e1142cbd10c3d057ca
DavidVela1712 <75803006+DavidVela1712@users.noreply.github.com>
2025-07-11 19:17:43 +0000

    110725-Fin-Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { ImageLink } from "./components/ImageLink";
+import { CloseButton, Col, Container, Row } from "react-bootstrap";
+import { useState } from "react";
+
+let nextId = 0;
 
 export const App = () => {
+
+  const [frase, setFrase] = useState('');
+  const [lista, setLista] = useState([]);
+
+  const añadirLista = () => {
+    setLista([...lista, {id: nextId++, frase}]);
+    setFrase('');    
+  };
+
+  const pulsarEnter = (e) => {
+    if(e.key === "Enter") {
+      añadirLista();
+    }
+  };
+
+  const eliminarLista = (id) => {
+    setLista(lista.filter(a => a.id !== id))
+  }
+
   return (
     <>
-      <div>
-        <ImageLink src={viteLogo} alt={"Vite logo"} href={"https://vite.dev"} />
-        <ImageLink
-          src={reactLogo}
-          alt={"React logo"}
-          href={"https://react.dev"}
-        />
-      </div>
-      <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container fluid className="d-flex flex-column justify-content-center align-items-center">
+        <Row>
+          <Col>
+            <h1>Escultores inspiradores:</h1>
+            <input
+              value={frase}
+              onChange={e => setFrase(e.target.value)}
+              onKeyDown={pulsarEnter}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+              {lista.map(e => (
+                <div><p key={e.id}>{e.frase}</p>
+                <CloseButton onClick={() => eliminarLista(e.id)}></CloseButton></div>
+              ))}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
